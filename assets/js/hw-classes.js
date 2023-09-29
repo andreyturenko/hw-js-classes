e8ee564b7f1992e97d4e5e0b52e2e3c03401806e
Andrey <andreyturenkoit@gmail.com>
2023-09-29 18:04:20 +0300

    Add homework task 2

--- assets/js/hw-classes.js
+++ assets/js/hw-classes.js
@@ -37,4 +37,103 @@ console.log("Age of the phone :>> ", iphone.calcPhoneAge());
 iphone.year = 2021;
 console.log("Year of manufacture :>> ", iphone.year);
 
-// //    *Для перевірки, чи належить рік виробництва певному діапазону, можна використати клас нижче.
+// // 2. *Реалізувати клас RangeValidator.
+// Клас призначений для валідації потрапляння числового значення в діапазон.
+// Границі діапазона є властивостями:
+// ■ from (типу number);
+// ■ to (типу number);
+// (from <= to)
+// // Реалізувати getter'и та setter'и для обох властивостей.
+
+class RangeValidator {
+  constructor(from, to) {
+    this._from = from;
+    this._to = to;
+    if (from > to) {
+      throw new RangeError("`from` must be less than or equal to `to`");
+    }
+  }
+  get from() {
+    return this._from;
+  }
+
+  set from(value) {
+    if (typeof value !== "number") {
+      throw TypeError("`from` must be a number");
+    }
+    if (value > this._to) {
+      throw RangeError("`from` value cannot be greater than `to` value");
+    }
+    this._from = value;
+  }
+
+  get to() {
+    return this._to;
+  }
+
+  set to(value) {
+    if (typeof value !== "number") {
+      throw TypeError("`to` must be a number");
+    }
+    if (value < this.from) {
+      throw RangeError("`to` value cannot be less than `from`");
+    }
+    this._to = value;
+  }
+  // // *Реалізувати getter range, який повертатиме масив із двома числами діапазону
+  //    (тобто геттер повертає не властивість, а масив із двома елементами, які є властивостями)
+  get range() {
+    return [this._from, this._to];
+  }
+  // // Реалізувати метод isValid, який прийматиме число і перевірятиме,
+  //    чи входить число у вказаний діапазон (повертає boolean).
+  isValid(number) {
+    if (typeof number !== "number") {
+      throw TypeError("input value must be a number");
+    }
+    return number >= this._from && number <= this._to;
+  }
+}
+
+// // Перевірки:
+// Конструктор (+сеттери)
+
+const range1 = new RangeValidator(1, 5.5);
+
+try {
+  const rangeChecking = new RangeValidator(10, 5.5);
+} catch (err) {
+  console.log(err);
+}
+
+// Робота сетерів
+
+range1.from = 5;
+
+try {
+  range1.from = 200;
+} catch (err) {
+  console.log(err);
+}
+
+range1.to = 80;
+
+try {
+  range1.to = -55;
+} catch (err) {
+  console.log(err);
+}
+
+// Робота гетерів
+
+console.log(range1.from);
+console.log(range1.to);
+
+// Робота геттера range
+
+console.log(range1.range);
+
+// Робота validate
+
+console.log(range1.isValid(10));
+console.log(range1.isValid(100));
